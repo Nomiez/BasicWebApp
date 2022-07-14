264a5d9bb428a9b8dc01545952bc55e6fd7a11f6
Simon Okutan <simon.ok@gmx.de>
2022-07-14 12:54:43 +0200

    Add addition

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,19 +2,35 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
-        if (query.contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "Simson";
-        } else { // TODO extend the programm here
-            return "";
-        }
+        try {
+            query = query.toLowerCase();
+            if (query.contains("shakespeare")) {
+                return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                        "English poet, playwright, and actor, widely regarded as the greatest " +
+                        "writer in the English language and the world's pre-eminent dramatist.";
+            } else if (query.contains("name")) {
+                return "Simson";
+            } else if (query.contains("plus")) {
+                String[] s = query.split(" ");
+
+                AtomicInteger sum = new AtomicInteger();
+                Arrays.stream(s).forEach(t -> {
+                            try {
+                                sum.addAndGet(Integer.parseInt(t));
+                            } catch (Exception ignored) {}
+                        }
+                );
+            } else if (query.contains("largest")) { // TODO extend the programm here
+                return "";
+            }
+        } catch (Exception ignored) {}
+        return "";
     }
 }
